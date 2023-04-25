f403347e6e62f3a6b89013c336436710982ffc73
Fabricio Frade <fabricio.frade@sga.pucminas.br>
2023-04-25 12:21:34 -0300

    Alterar código de movimentação para rodar em torno do cenário

---
+++ Assets/Scripts/CircleMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float radius = 5f;
+    public float verticalSpeed = 2f;
+    private float angle = 0f;
+    private float height = 0f;
+    private float centerZ;
+    private float centerX;
+    private Vector3 center;
+
+    void Start()
+    {
+        centerX = GameObject.Find("Scenary1").transform.position.x;
+        centerZ = GameObject.Find("Scenary1").transform.position.z;
+        center = new Vector3(centerX, 0f, centerZ);
+    }
+
+    void Update()
+    {
+        // Horizontal movement
+        angle += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float x = Mathf.Cos(angle) * radius;
+        float z = Mathf.Sin(angle) * radius;
+
+        // Vertical movement
+        height += Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
+        float y = height;
+
+        // Set position
+        transform.position = center + new Vector3(x, y, z);
+    }
+}
