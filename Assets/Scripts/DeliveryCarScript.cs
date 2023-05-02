90614eb66f5d8eb186c4306e9d6b4f4966392834
Ian <ian.wb1@hotmail.com>
2023-05-02 18:36:27 -0300

    demo de apresentação do dia 28/04/2023

---
+++ Assets/Scripts/DeliveryCarScript.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCarScript : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 0.01f;
+    //[SerializeField] float rotationSpeed = 1f;
+    [SerializeField] private int healthPoints = 1;
+
+    [Header("Bounds")]
+    [SerializeField]
+    private BoxCollider2D playerBounds;
+
+     [Header("Shoot")]
+    [SerializeField]
+    private Transform shootPivot;
+
+    [SerializeField]
+    private GameObject shootPrefab;
+
+    [Header("Delay")]
+    [SerializeField]
+    [Range(0f, 10f)]
+    private float initialDelay = 1f;
+
+    [SerializeField]
+    [Range(0f, 10f)]
+    private float spawnDelay = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Move();
+
+        ApplyBounds();
+
+    }
+    private void Move()
+        {
+            var h = Input.GetAxis("Horizontal");
+            var v = Input.GetAxis("Vertical");
+            
+
+            Vector3 movementDirection = new Vector3(h, 0, 0);
+
+            movementDirection.Normalize();
+            /*if (movementDirection != Vector3.zero){
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            }*/
+            
+
+            if (movementDirection != Vector3.zero)
+            {
+                transform.forward = movementDirection;
+            }
+
+            var move = new Vector3(
+                0f,
+                v * moveSpeed * Time.deltaTime,
+                0f
+            );
+
+            transform.Translate(move);
+        }
+
+        private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+
+            Destroy(other.gameObject);
+
+            // Update Health Points
+
+            healthPoints--;
+
+            // Check if Health Points is below 0 to destroy it
+
+            if (healthPoints <= 0)
+            {
+
+                Application.Quit();
+            }
+        }
+    }
+
+        private void ApplyBounds()
+    {
+        var minX = -playerBounds.bounds.extents.x + playerBounds.offset.x + playerBounds.transform.position.x;
+        var maxX = playerBounds.bounds.extents.x + playerBounds.offset.x + playerBounds.transform.position.x;
+
+        var minY = -playerBounds.bounds.extents.y + playerBounds.offset.y + playerBounds.transform.position.y;
+        var maxY = playerBounds.bounds.extents.y + playerBounds.offset.y + playerBounds.transform.position.y;
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, minX, maxX),
+            Mathf.Clamp(transform.position.y, minY, maxY),
+            transform.position.z
+        );
+    }
+
+    private void Awake()
+    {
+        InvokeRepeating(nameof(Shoot), initialDelay, spawnDelay);
+    }
+    private void Shoot()
+    {
+        
+
+        // Shoot
+
+        Instantiate(shootPrefab, shootPivot.position, shootPivot.rotation);
+
+    }
+
+    
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -102,7 +103,7 @@ public class Player : MonoBehaviour
             if (healthPoints <= 0)
             {
 
-                Application.Quit();
+                SceneManager.LoadScene( SceneManager.GetActiveScene().name );
             }
         }
     }
